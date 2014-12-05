d2a5a3f267941b2812e694d2023b13845c5c2247
Rob Best <robertbest89@gmail.com>
2014-12-05 23:15:27 +0000

    Initial commit

---
+++ PrototypeScripts/MeleeSystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeSystem : MonoBehaviour {
+	public float theDamage; // 
+	public float theDistance; //
+	public float theRadius; //
+	public KeyCode attackKey; //
+	public KeyCode attackKeyUp; //
+	public KeyCode attackKeyDown; //
+	public KeyCode attackKeyLeft; //
+	public KeyCode attackKeyRight; //
+	public Material[] materials; // 0 normal, 1 attack
+
+
+		
+	// Update is called once per frame
+	void Update () {
+		gameObject.renderer.material = materials[0];
+		if (Input.GetKeyDown (attackKey))
+		{
+
+			if (Input.GetKey (attackKeyUp))
+			{
+				DoMelee (materials[1], transform.position, collider2D.bounds.size, 0f, transform.up, theDistance, theDamage);
+			}
+			if (Input.GetKey (attackKeyDown))
+			{
+				DoMelee (materials[1], transform.position, collider2D.bounds.size, 0f, -transform.up, theDistance, theDamage);
+			}
+			else
+			{
+				DoMelee (materials[1], transform.position, new Vector2 (collider2D.bounds.size.x, collider2D.bounds.size.y * 1.3f), 0f, transform.right, theDistance, theDamage);
+			}	
+		}
+
+	}
+
+	// Boxcast 
+	void DoMelee (Material skin, Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, float damage)
+	{
+		gameObject.renderer.material = skin;
+		RaycastHit2D hit = Physics2D.BoxCast(origin, size, angle, direction, distance);
+		if (hit)
+		{
+			hit.transform.SendMessage ("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
+
---
+++ PrototypeScripts/PlayerController.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Set these in Unity
+	public float speed; //Player move speed
+	public int kickForce; // Force of kick
+	public int jumpLimit; // Number of jumps allowed before having to return to the floor
+	public int jumpHeight; //Jump Height
+	public KeyCode moveRight; // Right
+	public KeyCode moveLeft; // Left
+	public KeyCode moveJump; // Jump
+	public KeyCode moveDash; // Dash
+	public Material[] materials; //0 = Strong, 1 = Weak, 2 = Attack
+	//public bool strong; // TODO: Is the character weak or strong? // SWITCH MECHANIC
+	
+	// Private
+	private int jumpsMade; //Number of jumps performed since leaving the ground
+	private int fixedJumps; //Save the user-set jumpLimit
+	private float fixedSpeed; //Save the user-set fixedSpeed
+	private int matreset; // TODO: The current material for the object, 0 = Strong, 1 = Weak // SWITCH MECHANIC
+	//private bool readyToKick; // TODO: Is object able to attack? // KICK MECHANIC
+
+	// At start we set the number of jumps performed since leaving the ground to 0
+	void Start () {
+		jumpsMade = 0;
+		matreset = 0;
+
+		// TODO: Uncomment for switch mechanic
+		//readyToKick = true;
+		//fixedJumps = jumpLimit;
+		//fixedSpeed = speed;
+		//ChangeMode ();
+	}
+	
+	void Update () 
+	{
+		//Move Right
+		if (Input.GetKey (moveRight)) 
+		{
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,0); 
+		}
+		//Move Left
+		if (Input.GetKey (moveLeft)) 
+		{
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+			transform.eulerAngles = new Vector3(0,0,180); //flip the character on its x axis
+		}
+		//Jump
+		if (Input.GetKeyDown (moveJump))
+		{
+			if (jumpsMade < jumpLimit)
+			{
+				rigidbody2D.AddForce (Vector2.up * jumpHeight);
+				jumpsMade++;
+			}
+		}
+
+		// TODO: Uncomment to enable dash mechanic
+		// Add left or right force dependent on rotation, swap red 'attack' texture in
+		/*if (strong == true)
+		{
+			if (Input.GetKeyDown (moveDash)) 
+			{
+				if (transform.eulerAngles.z < 170 & readyToKick == true)
+				{ 
+					readyToKick = false;
+					renderer.material = materials[2];
+					rigidbody2D.AddForce (Vector3.right * kickForce);
+				}
+				if (transform.eulerAngles.z > 170 & readyToKick == true)
+				{
+					readyToKick = false;
+					renderer.material = materials[2];
+					rigidbody2D.AddForce (Vector3.left * kickForce);
+				}
+			}
+		}*/	
+	}
+	
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		// If red orton hits Cena enemy: destroy Cena
+		if (gameObject.renderer.material.name == "Angry (Instance)" & (col.gameObject.name == "Enemy" || col.gameObject.name == "Enemy(Clone)")) 
+		{
+			Destroy(col.gameObject);
+		}
+		// If Cena enemy hits normal Orton: destroy Orton, restart level
+		else if (col.gameObject.tag == "enemy") 
+		{
+			Destroy(gameObject);
+			Application.LoadLevel(Application.loadedLevel);
+		}
+		// If Orton hits a piece of floor, reset the available jumps and switch back to normal texture 
+		else if (col.gameObject.tag == "Ground") 
+		{
+			jumpsMade = 0;
+			renderer.material = materials[matreset];
+		}
+	}
+	
+	// TODO: Uncomment for dash mechanic
+	// Make sure dash/kick is always available after leaving a bit of floor
+	/*void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Ground") 
+		{
+			readyToKick = true;
+		}
+	}*/
+
+	// TODO: Uncomment for strong/weak mechanic
+	/*public void ChangeMode()
+	{
+		if (strong == true) 
+		{
+			// Weak
+			matreset = 1;
+			renderer.material = materials[1];
+			jumpLimit = 1;
+			speed = speed/3;
+			strong = false;
+			gameObject.tag = "weak";
+		}
+		else 
+		{
+			// Strong
+			matreset = 0;
+			renderer.material = materials[0];
+			jumpLimit = fixedJumps;
+			speed = fixedSpeed;
+			strong = true;
+			gameObject.tag = "strong";
+		}
+	}*/
+}
+
+
+

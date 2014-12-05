3c3a750a6e02cc4bb351f6c9aaec1018ffe28337
Rob Best <robertbest89@gmail.com>
2014-12-05 23:37:42 +0000

    Cleaned up and commented

--- PrototypeScripts/MeleeSystem.cs
+++ PrototypeScripts/MeleeSystem.cs
@@ -2,19 +2,21 @@
 using System.Collections;
 
 public class MeleeSystem : MonoBehaviour {
-	public float theDamage; // 
-	public float theDistance; //
-	public float theRadius; //
-	public KeyCode attackKey; //
-	public KeyCode attackKeyUp; //
-	public KeyCode attackKeyDown; //
-	public KeyCode attackKeyLeft; //
-	public KeyCode attackKeyRight; //
+	// Set in Unity
+	public float theDamage; // Amount of damage done with each attack
+	public float theDistance; // Range/distance of the boxcast/the attack
+	public KeyCode attackKey; // The key to attack
+	public KeyCode attackKeyUp; // The up key
+	public KeyCode attackKeyDown; // The down key
+	public KeyCode attackKeyLeft; // The left key
+	public KeyCode attackKeyRight; // The right key
 	public Material[] materials; // 0 normal, 1 attack
 
 
 		
-	// Update is called once per frame
+	// Press attackKeyUp to attack in the direction you're facing
+	// We do a directional attack if Up or Down are held !!! CURRENTLY NOT WORKING IF THE CHARACTER TIPS OVER !!!
+	// ^ TODO: Possible workaround would be to flip the character back to Y orientation on hitting the ground 
 	void Update () {
 		gameObject.renderer.material = materials[0];
 		if (Input.GetKeyDown (attackKey))
@@ -36,7 +38,7 @@ public class MeleeSystem : MonoBehaviour {
 
 	}
 
-	// Boxcast 
+	// Boxcast melee attack method 
 	void DoMelee (Material skin, Vector2 origin, Vector2 size, float angle, Vector2 direction, float distance, float damage)
 	{
 		gameObject.renderer.material = skin;

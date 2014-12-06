8d0e93e63d51df8088d52ed24595c3856eb637e3
Hobomechanistic <dejagerrob@gmail.com>
2014-12-06 15:05:39 +0000

    Added UI scripts
    Added UI Scripts

---
+++ UIScripts/ClickPause.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickPause : MonoBehaviour {
+
+	GameObject tmpStoreObj;
+
+	// Use this for initialization
+	void Start () {
+		//GameObject.Find("Panel").SetActive(true);
+		tmpStoreObj = GameObject.Find("Panel");
+		tmpStoreObj.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void togglePause(){
+		if(tmpStoreObj.activeSelf){
+			tmpStoreObj.SetActive(false);
+			Time.timeScale = 1F;
+		}else{
+			tmpStoreObj.SetActive(true);
+			Time.timeScale = 0F;
+		}
+	}
+}

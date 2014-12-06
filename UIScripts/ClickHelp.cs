70c3147031445420f2c332d0c1f99ce2368dcfa9
Hobomechanistic <dejagerrob@gmail.com>
2014-12-06 16:58:41 +0000

    Added UI
    Added Quit and Help to pause menu

---
+++ UIScripts/ClickHelp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickHelp : MonoBehaviour {
+
+	GameObject storeCtrls;
+
+	// Use this for initialization
+	void Start () {
+		storeCtrls = GameObject.Find("PnlHelp");
+		storeCtrls.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void toggleHelp(){
+		if(storeCtrls.activeSelf){
+			storeCtrls.SetActive(false);
+			//Time.timeScale = 1F;
+		}else{
+			storeCtrls.SetActive(true);
+			//Time.timeScale = 0F;
+		}
+	}
+}

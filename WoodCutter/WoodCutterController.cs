2792a7a843009086e81f4f06566bfadd72bd8d7d
Rob Best <robertbest89@gmail.com>
2014-12-07 05:04:14 +0000

    Final commit of the night

--- WoodCutter/WoodCutterController.cs
+++ WoodCutter/WoodCutterController.cs
@@ -6,17 +6,51 @@ public class WoodCutterController : MonoBehaviour {
 	public Camera camera;
 	public float sightdistance;
 	public float chance;
-	public GameObject bbw;
-	public GameObject lrrh;
-	
-
+	public int timeout;
+	private int startTimeOut;
+	public int losIncrease;
 
 	private bool headingright;
+	private int startSpeed;
 	
+	void Start() {
+		startSpeed = speed;
+		startTimeOut = timeout;
 
+	}
 	// Update is called once per frame
 	void Update () {
+		timeout--;
+		SpeedUpOnLOS ();
+		KeepOnMoving ();
+		FallThroughFloor ();
+	}
+	
+	// Woodcutter will only fall through the floor if he is above BBW and there is a platform to land on
+	// There is also a slight timeout on it.
+	void FallThroughFloor ()
+	{
+		GameObject bbw = GameObject.FindGameObjectWithTag ("BBW");
 
+		float bbwdiffx = bbw.transform.position.x - transform.position.x;
+		float bbwdiffy = transform.position.y - bbw.transform.position.y;
+
+		if ((bbwdiffx > -10 & bbwdiffx < 10) & bbwdiffy > 2 & timeout <= 0)
+		{
+			RaycastHit2D[] hit = Physics2D.RaycastAll (transform.position, -Vector2.up, sightdistance);
+
+			if (hit.Length >= 5 & hit[1].collider.tag == "PlatformTrigger") 
+			{
+				Physics2D.IgnoreCollision(gameObject.collider2D, hit[2].rigidbody.collider2D);
+				timeout = startTimeOut;
+			}
+		}
+	}
+
+	// Woodsman moves along the X axis until he reaches near the camera's edge, 
+	// then he turns around and goes the other way
+	void KeepOnMoving ()
+	{
 		transform.eulerAngles = new Vector3(0,0,0);
 		Vector3 cam = camera.WorldToScreenPoint(transform.position);
 		if (cam.x < 100)
@@ -28,7 +62,7 @@ public class WoodCutterController : MonoBehaviour {
 			headingright = false;
 			transform.Translate (-Vector2.right * speed * Time.deltaTime);
 		}
-
+		
 		if (headingright == true)
 		{
 			transform.Translate (Vector2.right * speed * Time.deltaTime);
@@ -37,20 +71,42 @@ public class WoodCutterController : MonoBehaviour {
 		{
 			transform.Translate (-Vector2.right * speed * Time.deltaTime);
 		}
-
-		FallThroughFloor ();
-
-
 	}
 
-	void FallThroughFloor ()
-	{
-		float bbwdiff = bbw.transform.position;
-		RaycastHit2D[] hit = Physics2D.RaycastAll (transform.position, -Vector2.up, sightdistance);
-		Debug.Log (hit.Length);
-		if (hit.Length >= 5 & hit[1].collider.tag == "PlatformTrigger") 
+	//Woodsman will increase speed if he sees the BBW
+	void SpeedUpOnLOS() {
+		if (headingright == true)
 		{
-			Physics2D.IgnoreCollision(gameObject.collider2D, hit[2].rigidbody.collider2D);
+			RaycastHit2D[] hit = Physics2D.RaycastAll (transform.position, Vector2.right, sightdistance);
+			for (int i = 0; i < hit.Length; i++)
+			{
+				if (hit[i].collider.tag == "BBW")
+				{
+					speed = speed * losIncrease;
+					break;
+				}
+				else 
+				{
+					speed = startSpeed;
+				}
+			}
 		}
+		else if (headingright == false)
+		{
+			RaycastHit2D[] hit = Physics2D.RaycastAll (transform.position, -Vector2.right, sightdistance);
+			for (int i = 0; i < hit.Length; i++)
+			{
+				if (hit[i].collider.tag == "BBW")
+				{
+					speed = speed * losIncrease;
+					break;
+				}
+				else 
+				{
+					speed = startSpeed;
+				}
+			}
+		}
+
 	}
 }

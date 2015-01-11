fe8faceb841abd7d2f2fd720839effc9d027e4f8
Rob Best <robertbest89@gmail.com>
2015-01-11 23:58:57 +0000

    Fixed the Victory point
    - Changed the collider on the Victory prefab to a circle
    - Made it so that both LRRH and BBW must be in contact with the Victory
    object for the level to end

--- Assets/Resources/Scripts/LevelLoad/LoadNextLevel.cs
+++ Assets/Resources/Scripts/LevelLoad/LoadNextLevel.cs
@@ -6,19 +6,46 @@ public class LoadNextLevel : MonoBehaviour {
 	//Running out of time so I'm not using GetComponent camera I'm just going to plug it in here
 	public Camera mainCam;
 
+
+	private bool bbw;
+	private bool lrrh;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		bbw = false;
+		lrrh = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (lrrh == true && bbw == true)
+		{
+			mainCam.transform.SendMessage("startwaitThenPanUp", SendMessageOptions.DontRequireReceiver);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if(other.name == "BBW" || other.name == "LRRH"){
-			mainCam.transform.SendMessage("startwaitThenPanUp", SendMessageOptions.DontRequireReceiver);
+		if (other.name == "BBW")
+		{
+			bbw = true;
+		}
+
+		if (other.name == "LRRH")
+		{
+			lrrh = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.name == "BBW")
+		{
+			bbw = false;
+		}
+		
+		if (other.name == "LRRH")
+		{
+			lrrh = false;
 		}
 	}
 }
